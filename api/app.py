44a3609623e599eb1880d5c3f27ff4f7a7fad3bb
katGhost <katghost124@gmail.com>
2026-01-08 14:14:59 +0200

    Add wsgi

---
+++ api/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from api.extensions import db, migrate
+from api.routes.core import core
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    CORS(app)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(core)
+
+    return app
