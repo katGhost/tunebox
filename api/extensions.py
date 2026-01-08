17efaa3742ee8aba9195502d220e60f2bc3cf2c5
katGhost <katghost124@gmail.com>
2026-01-08 14:13:26 +0200

    refactor: env vars and extensions correction

--- api/extensions.py
+++ api/extensions.py
@@ -1,8 +1,5 @@
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import declared_attr
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+migrate = Migrate()
