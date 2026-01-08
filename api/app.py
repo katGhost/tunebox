b285471b32b248b555f21ef9cdc4e4cd1089be76
katGhost <katghost124@gmail.com>
2026-01-08 14:25:46 +0200

    refactor: running app

--- api/app.py
+++ api/app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from api.extensions import db, migrate
+from api.extensions import db, migrate, jwt
 from api.routes.core import core
 
 def create_app():
     app = Flask(__name__)
 
+    # configurations
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///app.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -15,6 +16,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     app.register_blueprint(core)
 
