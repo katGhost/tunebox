22c2070b2bb0186acfdd273b33ab0766e7717da0
katGhost <katghost124@gmail.com>
2026-01-08 13:41:13 +0200

    routing fixed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,23 +3,23 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-    const [data, setData] = useState(null);
+  const [message, setMessage] = useState("loading...");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch('http://localhost:8080/api/home'); // Adjust the endpoint based on your Flask app
-            const result = await res.json();
-            setData(result);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:8080/api/home');
+      const result = await res.json();
+      setMessage(result.message);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    if (!data) return <div>Loading...</div>;
+  if (!message) return <div>Loading...</div>;
 
-    return (
-        <div>
-          <h1>{JSON.stringify(data)}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
 }
