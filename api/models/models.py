2eebabe200ad2af150b6097e14e048f8e6ce59ca
katGhost <katghost124@gmail.com>
2026-01-10 17:21:21 +0200

    db: add music corresponded models

--- api/models/models.py
+++ api/models/models.py
@@ -15,6 +15,48 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f'<User: {self.username}>'
-    
+
+
+"""Artist"""
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_name = db.Column(db.String(255), unique=True, nullable=False)
+
     # Relationships
-    ...
+    albums = db.relationship('Album', back_populates='artist', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Artist: {self.artist_name}>'
+
+
+"""Album"""
+class Album(db.Model):
+    __tablename__ = 'albums'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=False, nullable=False)
+    artist = db.Column(db.String(255), db.ForeignKey('artist_name'))
+    release_year = db.Column(db.Integer)
+
+    # Relationships
+    tracks = db.relationship(
+        "Track",
+        back_populates="album",
+        cascade="all, delete-orphan",
+        order_by="Track.track_number"
+    )
+    artist = db.relationship('Artist', back_populates='album')
+
+class Track(db.Model):
+    __tablename__ = "tracks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    duration_seconds = db.Column(db.Integer)
+    track_number = db.Column(db.Integer)
+
+    # Relationships
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=False)
+    album = db.relationship("Album", back_populates="tracks")
