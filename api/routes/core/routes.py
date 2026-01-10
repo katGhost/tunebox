66ac3736375bbc77fb17a311d2aab204e5c56a7c
katGhost <katghost124@gmail.com>
2026-01-10 17:20:08 +0200

    modify: route works

--- api/routes/core/routes.py
+++ api/routes/core/routes.py
@@ -1,8 +1,8 @@
-from flask import request, render_template, redirect, url_for, session, g, jsonify
-from . import core
+from flask import request, redirect, url_for, session, g, jsonify
+from api.routes.core import core
 
 @core.route('/api/home', methods=['GET'])
 def index():
     """Home page -> redirects based on auth status"""
-    return jsonify({'message': 'Welcome User'})
+    return jsonify({'message': 'Welcome User!'})
 
