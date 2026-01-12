80b669a6213bea7a5729b15f0fde4dbc91dd69d0
katGhost <katghost124@gmail.com>
2026-01-12 17:17:20 +0200

    backend: add jwt_requried to dashboard

--- api/routes/core/routes.py
+++ api/routes/core/routes.py
@@ -1,10 +1,16 @@
 from flask import request, redirect, url_for, session, g, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from api.routes.core import core
+from api.models.models import User
+
 
 @core.route('/api/home', methods=['GET'])
+@jwt_required()
 def index():
     """Home page -> redirects based on auth status"""
-    return jsonify({'message': 'Welcome User!'})
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({'message': f'Welcome User! {user}'})
 
 
 """Create an endpoint for navifgation bar"""
