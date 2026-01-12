e5ae58f1cbaca6c15ba9ea0f101c8b9aba80c211
katGhost <katghost124@gmail.com>
2026-01-12 16:51:53 +0200

    basic auth

--- api/routes/auth/routes.py
+++ api/routes/auth/routes.py
@@ -4,7 +4,7 @@ from api.models.models import User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
 from api.routes.auth import auth
 
-@auth.route('/get_name', methods=['GET'])
+@auth.route('/users', methods=['GET'])
 @jwt_required()
 def get_name():
     #username = request.form.get('')
@@ -13,7 +13,7 @@ def get_name():
 
     #check if any user exists
     if not user:
-        return redirect(url_for('auth.unauthorized')), 302
+        return jsonify({'msg': 'No user added yet'})
     else:
         return jsonify({'msg': 'User found', 'username': User.username})
 
@@ -21,37 +21,45 @@ def get_name():
 # Register/Signup route
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'GET':
+        username = 'Kat'
+        user = User.query.where(User.username==username).first()
+        return jsonify({'msg': 'endpoint alive!', 'user': user})
     if request.method == 'POST':
-        data = request.get_json()
-        username = data['username']
-        email = data['email']
-        password = data['password']
-        confirm_password = data['confirm-password']
+        data = request.get_json() or {}
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
         print('Received data:', username , password)
 
-        if not username or not email:
-            return jsonify({'msg': 'Required fileds!'})
-        
-        # passwrod must match password re-entry
-        if password != confirm_password:
-            return jsonify({'msg': 'Password must match!'})
+        if not username or not email or not password:
+            return jsonify({'msg': 'All fields are required!'}), 400
         
         # look for user in db is user exists -> look by username
         user = User.query.filter_by(username=username).first()
+        if user:
+            return jsonify({'msg': 'User already exists!'}), 409
         
         # hash
         hash = bcrypt.generate_password_hash(password).decode("utf-8")
-        
+        access_token = create_access_token(User.id)
         
         # create new user
-        new_user = User(username=username, email=email, password_hash=hashed) #type: ignore
+        new_user = User(username=username, email=email, password_hash=hash) #type: ignore
 
         # add new user to the db
         db.session.add(new_user)
         db.session.commit()
     
     # Get request
-    return jsonify({'msg': 'You need to sign in'})
+    return jsonify({
+        "success": True,
+        "user": {
+            "id": new_user.id,
+            "username": new_user.username,
+            "email": new_user.email,
+        }
+    }), 201
 
 """Login"""
 
