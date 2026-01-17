c9f6f2356c36ecc54148da251f0d6cbcd0b0816d
katGhost <katghost124@gmail.com>
2026-01-17 09:45:13 +0200

    client: add logout -> remove access token

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,13 +1,27 @@
 'use client'
 
-import { useEffect, useState } from 'react';
 import api from '../../lib/api';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { logout } from '../../lib/auth';
+
 
 export default function Dashboard() {
-  const [message, setMessage] = useState("loading...");
-  const [error, setError] = useState('');
+  const [message, setMessage] = useState<string>("loading...");
+  const [error, setError] = useState<string>('');
+  const [checkingAuth, setCheckingAuth] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
+    const token = localStorage.getItem("access_token");
+
+    // No token? -> redirect user immediately to login
+    if (!token) {
+      router.push("/auth/login");
+      return;
+    }
+
+    // token exists, backend will verify
     const fetchUserData = async () => {
       try{
         const res = await api.get('/api/home');
@@ -15,22 +29,42 @@ export default function Dashboard() {
       }
       catch (err: any)
       {
-        setError(err.response?.data.msg || "Unauthorized");
+        // if token is invalid or expired
+        if (err?.response.status == 401) {
+          localStorage.removeItem("access_token");  // remove token
+          router.push("/auth/login"); // redirect user to login
+        }
+        else {
+          setError("Something went wrong!");
+        }
       }
     };
 
     fetchUserData();
-  }, []);
+  }, [router]);
 
-  // handle data loading state
-  if (!message) return <div>Loading...</div>;
+  // check auth session
+  if (checkingAuth) return <p>Checking session...</p>
 
   // handle error
   if (error) return <p>{error}</p>
 
   return (
-    <div>
-      <h1>{message}</h1>
+    <div className="w-full mx-auto px-4 py-6">
+      <div className="flex">
+        <h1>{message}</h1>
+
+        {/* Button logout */}
+        <button
+          onClick={() => {
+            logout();
+            router.push('/auth/login');
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
+    
   );
 }
